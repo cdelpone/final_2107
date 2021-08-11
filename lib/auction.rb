2d607de8c5478d6e244e935a5977bbc658824095
Christina Delpone <cdelpone@gmail.com>
2021-08-11 10:21:42 -0600

    Compelte iteration2

--- lib/auction.rb
+++ lib/auction.rb
@@ -1,5 +1,4 @@
 class Auction
-
   attr_reader :items
 
   def initialize
@@ -7,12 +6,30 @@ class Auction
   end
 
   def add_item(item)
-    @items << item
+    items << item
   end
 
   def item_names
     items.map do |item|
       item.name
+    end
+  end
+
+  def unpopular_items
+    items.select do |item|
+      item.bids.empty?
+    end
+  end
+
+  def popular_items
+    items.select do |item|
+      !item.bids.empty?
+    end
+  end
+
+  def potential_revenue
+    popular_items.sum do |item|
+      item.current_high_bid
     end
   end
 
--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,19 @@
 class Item
-  attr_reader :name
+  attr_reader :name,
+              :bids
 
   def initialize(name)
     @name = name
+    @bids = Hash.new(0)
+  end
+
+  def add_bid(attendee, bid)
+    bids[attendee] = bid
+  end
+
+  def current_high_bid
+    bids.values.max_by do |value|
+      value
+    end
   end
 end
